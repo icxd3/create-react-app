4eadbabf8443f12cee151a4613203643d61c71d0
Cxd3 <icxd3@proton.me>
2026-06-11 20:00:43 +0300

    lecture about React elements and components ended

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// JSX - элементы, это синтаксическое расширение JavaScript, 
+// которое позволяет писать HTML-подобный код внутри JavaScript.
+// JSX не является строкой или HTML, 
+// это синтаксис, который преобразуется 
+// в вызовы JavaScript-функций.
+// JSX позволяет создавать React-элементы 
+// с помощью удобного и читаемого синтаксиса, 
+// который напоминает HTML, но при этом обладает 
+// мощью JavaScript.
+
+let Header = () => {
+  return <h2>Hello, React!</h2>
+}
+
+// let Field = () => {
+//   let holder = 'Enter here...';
+//   let styled = {
+//     width: '300px',
+//   };
+//   return <input placeholder={holder} type='text' style={styled}/>
+// }
+
+class Field extends React.Component {
+  render() {
+    let holder = 'Enter here...';
+    let styled = {
+      width: '300px',
+    };
+    return <input placeholder={holder} type='text' style={styled}/> 
+  }
+}
+
+// function Button() {
+//   let text = 'Log in';
+//   let logged = false;
+//   return <button>{logged ? 'Log out' : 'Log in'}</button>
+// }
+// function Button() {
+//   let text = 'Log in';
+//   let logged = false;
+//   return <button>{logged ? 'Log out' : 'Log in'}</button>
+// }
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header/>
+      <Field/>
     </div>
   );
 }
+
+export {Header};
 
 export default App;
